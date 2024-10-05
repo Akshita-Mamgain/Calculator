012a8a402d2bdd2628fb6ad1e0368c2e104c842e
Akshita-Mamgain <akshitamamgain@gmail.com>
2024-10-05 01:22:08 -0700

    Update

---
+++ java.js
@@ -0,0 +1,37 @@
+var ext="",num1,num2,op,opt;
+var data=document.getElementById("out");
+   function num(x)
+   {
+    data.value=ext+=x;
+   }
+
+
+   function oper(op)
+   {
+    num1=parseFloat(data.value);
+    data.value="";
+    ext="";
+    opt=op;
+   }
+
+
+       function result()
+       {
+        num2=parseFloat(data.value);
+        switch(opt)
+        {
+            case "+":
+                data.value=num1+num2;
+            break;
+
+            case "-":
+                data.value=num1-num2;
+            break;
+            case "/":
+                data.value=num1/num2;
+            break;
+            case "*":
+                data.value=num1*num2;
+            break;
+        }
+       }
